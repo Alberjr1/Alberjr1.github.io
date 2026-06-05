2e9056e5684e0e05d3698590b42d5b503e29d703
Alberto Ortega <alberortegacabrera@gmail.com>
2026-06-05 14:14:50 +0200

    Cambio en links

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -15,14 +15,14 @@ function Main() {
         <div className="content">
           <div className="social_icons">
             <a href="https://github.com/Alberjr1" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/alberto-ortega-cabrera" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.linkedin.com/in/alberto-ortega-cabrera-63b0641a1/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Alberto Ortega</h1>
           <p>Computer Engineering Student</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/Alberjr1" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/alberto-ortega-cabrera" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://www.linkedin.com/in/alberto-ortega-cabrera-63b0641a1/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
